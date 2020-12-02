5b918c2792c2a24bd90c3d115ef07c1cafad1b9c
8bitbran <brandonmar97@gmail.com>
2020-12-01 20:41:47 -0500

    commit tree 5df271cb4e6c659863ee604ad82a3863d7e54702

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('%c HI', 'color: firebrick')
+function fetchImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+    fetch(imgUrl)
+        .then(resp => resp.json())
+        .then(json => renderImages(json))
+}
+
+function renderImages(dogImages) {
+    const imgDiv = document.querySelector("#dog-image-container")
+    dogImages.message.forEach(image => {
+        const img = document.createElement("img")
+        img.src = image
+        imgDiv.appendChild(img)
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function(e) {
+    fetchImages()
+})
